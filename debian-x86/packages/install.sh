fde952306e465d8e15178979d75666d38660dbe3
Alexander Carr <jalexandercarr@gmail.com>
2023-06-28 18:34:54 -0400

    Added helm installation support

--- debian-x86/packages/install.sh
+++ debian-x86/packages/install.sh
@@ -55,6 +55,12 @@ echo "currently testing"
 # sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 # sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 
+# Helm
+# curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+# chmod 700 get_helm.sh
+# ./get_helm.sh
+# rm get_helm.sh
+
 # Cleaning up
 unset logfile
 set +x
